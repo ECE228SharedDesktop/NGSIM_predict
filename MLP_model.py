113552ad38d703be0c65bb3efb8c7061bdd17181
Lawson Fuller <llfuller@users.noreply.github.com>
2022-06-09 20:31:35 -0700

    Update MLP_model.py

--- MLP_model.py
+++ MLP_model.py
@@ -28,11 +28,12 @@ def build_mlp(
             super(NN, self).__init__()
             self.ourNN = nn.Sequential() # Make sequential module container
             self.ourNN.add_module("Hidden_0", nn.Linear(input_size,size,bias=True)) # Layer 1
-            self.ourNN.add_module("ReLU_0", nn.ReLU(inplace=False))
+            self.ourNN.add_module("LeakyReLU_0", nn.LeakyReLU(inplace=False))
             for n in range(1,n_layers):
                 self.ourNN.add_module("Hidden_"+str(n), nn.Linear(size, size, bias=True))
-                self.ourNN.add_module("ReLU_"+str(n), nn.ReLU(inplace=False))
+                self.ourNN.add_module("LeakyReLU_"+str(n), nn.LeakyReLU(inplace=False))
             self.ourNN.add_module("Output_layer",nn.Linear(size, output_size, bias=True))# Last layer
+            self.ourNN.add_module("Output_Softmax_activation",nn.Softmax())
             self.ourNN.to(device)
         def forward(self, x):
             x=self.ourNN(x)
@@ -67,11 +68,11 @@ def train_on_data(x, y, num_iterations, n_layers, size, num_channels, device):
     :param num_iterations: number of training iterations
     :return: tuple (MLP network object, array of training losses)
     """
-    input_size = 13
-    output_size = input_size
-    n_layers = 13
-    size = 30
-    num_channels = 1
+    # input_size = 13
+    # output_size = input_size
+    # n_layers = 13
+    # size = 30
+    # num_channels = 1
 
     input_data = np.random.random((input_size))
     target_data = np.random.random((output_size))
@@ -109,3 +110,47 @@ def train_on_data(x, y, num_iterations, n_layers, size, num_channels, device):
     # plt.ylabel("Log_10(Loss)")
     # plt.title("Loss per Training")
     # plt.show()
+
+
+# input_size = 13
+# output_size = input_size
+# n_layers = 13
+# size = 30
+# num_channels = 1
+#
+#
+# input_data = np.random.uniform(low=-1,high=1,size=input_size)
+# target_data = np.random.uniform(low=-1,high=1,size=output_size)
+#
+# input_data = np2torch(input_data)
+# target_data = np2torch(target_data)
+#
+# model = build_mlp(input_size, output_size, n_layers, size) # model
+# criterion = nn.MSELoss(reduction="none") # loss function
+# optimizer = torch.optim.SGD(model.parameters(), lr=0.01, momentum=0.9) # optimizer
+# model.train() #sets model to train mode (dropout enabled)
+# torch.set_grad_enabled(True) # L5kit does this, and I don't know why
+#
+# progress_bar = tqdm(range(1000))#cfg["train_params"]["max_num_steps"]))
+# losses_train = []
+#
+# for _ in progress_bar:
+#     model.eval()
+#     outputs = torch.reshape(model.forward(input_data.to(device)),target_data.size())
+#     print(outputs.size())
+#     loss = (criterion(outputs, target_data)).mean()
+#
+#     # Backward pass
+#     optimizer.zero_grad()
+#     loss.backward()
+#     optimizer.step()
+#
+#     losses_train.append(loss.item())
+#     progress_bar.set_description(f"loss: {loss.item()} loss(avg ): {np.mean(losses_train)}")
+#
+# plt.figure()
+# plt.plot(np.log10(np.array(losses_train)))
+# plt.xlabel("Training iteration")
+# plt.ylabel("Log_10(Loss)")
+# plt.title("Loss per Training")
+# plt.show()
