9589c28622b3f5164cc07dd72f68eab7a86afb40
Lawson Fuller <llfuller@users.noreply.github.com>
2022-06-09 22:39:36 -0700

    Update MLP_model.py

--- MLP_model.py
+++ MLP_model.py
@@ -13,7 +13,8 @@ def build_mlp(
           input_size=4,
           output_size=3,
           n_layers=2,
-          size=50):
+          size=50,
+          batch_size=10):
     """
     Args:
         input_size: int, dim(inputs)
@@ -24,7 +25,7 @@ def build_mlp(
         An instance of (a subclass of) nn.Module representing the network.
     """
     class NN(nn.Module):
-        def __init__(self, input_size, output_size, n_layers, size):
+        def __init__(self, input_size, output_size, n_layers, size, batch_size):
             super(NN, self).__init__()
             self.ourNN = nn.Sequential() # Make sequential module container
             self.ourNN.add_module("Hidden_0", nn.Linear(input_size,size,bias=True)) # Layer 1
@@ -35,10 +36,13 @@ def build_mlp(
             self.ourNN.add_module("Output_layer",nn.Linear(size, output_size, bias=True))# Last layer
             self.ourNN.add_module("Output_Softmax_activation",nn.Softmax())
             self.ourNN.to(device)
+
+            self.batch_size = batch_size
         def forward(self, x):
+            x = x.reshape((self.batch_size, 100, 4))
             x=self.ourNN(x)
             return x
-    return NN(input_size, output_size, n_layers, size)
+    return NN(input_size, output_size, n_layers, size, batch_size)
 
 def np2torch(x, cast_double_to_float=True):
     """
@@ -58,7 +62,7 @@ def send_all_to_device(list,device):
         thing.to(device)
 
 
-def train_on_data(x, y, num_iterations, n_layers, size, device):
+def train_on_data(x_all_batches, y_all_batches, num_iterations, n_layers, size, batch_size, num_epochs, device):
     """
     :param x: inputs to MLP
     :param y: desired outputs of MLP
@@ -74,38 +78,50 @@ def train_on_data(x, y, num_iterations, n_layers, size, device):
     # size = 30
     # num_channels = 1
 
-    input_data = x
-    target_data = y
-
-    input_data = np2torch(input_data)
-    target_data = np2torch(target_data)
-
-    input_size = input_data.size()
-    output_size = target_data.size()
-
-    model = build_mlp(input_size, output_size, n_layers, size) # model
-    criterion = nn.CrossEntropyLoss(reduction="none") # loss function
-    optimizer = torch.optim.SGD(model.parameters(), lr=0.01, momentum=0.9) # optimizer
-    model.train() #sets model to train mode (dropout enabled)
-    torch.set_grad_enabled(True) # L5kit does this, and I don't know why
-
-    progress_bar = tqdm(range(num_iterations))#cfg["train_params"]["max_num_steps"]))
+    progress_bar = tqdm(range(num_epochs))#cfg["train_params"]["max_num_steps"]))
+    input_data = x_all_batches
+    target_data = y_all_batches
     losses_train = []
 
-    for _ in progress_bar:
-        model.eval()
-        outputs = torch.reshape(model.forward(input_data.to(device)),target_data.size())
-        loss = (criterion(outputs, target_data)).mean()
+    for _ in tqdm(range(num_epochs)):
+        np.random.shuffle(input_data)
+        np.random.shuffle(target_data)
 
-        # Backward pass
-        optimizer.zero_grad()
-        loss.backward()
-        optimizer.step()
+        input_data = np2torch(input_data)
+        target_data = np2torch(target_data)
 
-        losses_train.append(loss.item())
-        progress_bar.set_description(f"loss: {loss.item()} loss(avg ): {np.mean(losses_train)}")
+        input_size = input_data.size()[-1]
+        output_size = target_data.size()[-1]
 
-    return (model, losses_train)
+        num_batches = int(input_data.size()[0] / batch_size)
+
+        model = build_mlp(input_size, output_size, n_layers, size, batch_size)  # model
+        print(model)
+        criterion = nn.CrossEntropyLoss(reduction="none") # loss function
+        optimizer = torch.optim.SGD(model.parameters(), lr=0.01, momentum=0.9) # optimizer
+        model.train() #sets model to train mode (dropout enabled)
+        torch.set_grad_enabled(True) # L5kit does this, and I don't know why
+
+        # progress_bar = tqdm(range(num_batches))#cfg["train_params"]["max_num_steps"]))
+
+        for i in range(num_batches):
+            input_data_batch = input_data[i*batch_size:(i+1)*batch_size]
+            target_data_batch = target_data[i*batch_size:(i+1)*batch_size]
+            model.eval()
+            # print("Shape of model.forward(input_data.to(device)):"+str((model.forward(input_data.to(device))).size()))
+            # print("Shape of target data: "+str(target_data.size()))
+            outputs = torch.reshape(torch.mean(model.forward(input_data_batch.to(device)),dim=1),target_data_batch.size())
+            loss = (criterion(outputs, target_data_batch)).mean()
+
+            # Backward pass
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
+
+            losses_train.append(loss.item())
+            progress_bar.set_description(f"loss: {loss.item()} loss(avg ): {np.mean(losses_train)}")
+
+        return (model, losses_train)
 
     # plt.figure()
     # plt.plot(np.log10(np.array(losses_train)))
